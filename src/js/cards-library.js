b909794bf4a4844ab767016a6417efe6e7396cd7
figatix <60873339+figatix@users.noreply.github.com>
2023-02-07 22:49:45 +0200

    Update cards-library.js
    add link and data-attrs

--- src/js/cards-library.js
+++ src/js/cards-library.js
@@ -3,9 +3,11 @@ export default function createCardsLibrary(arrOfCards) {
     .map(el => {
       return `
       <li class="card">
-        <img class="card__img" src="{./img/Rectangle 5.jpg}" alt="{poster}" loading="lazy">
-        <h2 class="card__title">{GREYHOUND}</h2>
-        <p class="card__text">{Drama, Action} | {2020}<span class="card__text_rating">{10.0}</span></p>
+        <a class="card__link" href="{original}" data-id="{1234}">
+          <img class="card__img" src="./images/logo.png" alt="{poster}" data-source="{original}"  loading="lazy">
+          <h2 class="card__title">{GREYHOUND}</h2>
+          <p class="card__text">{Drama, Action} | {2020}<span class="card__text_rating">{10.0}</span></p>
+        </a>
       </li>`;
     })
     .join('');
