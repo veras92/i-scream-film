24db7a25509a926c9a1c043aee9ff6442776a4cd
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-02-07 23:07:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './partials/js/homepage';
 import './partials/js/main';
+import './partials/js/library';
 import './partials/js/modal-window';
 import './partials/js/authentication';
 import './partials/js/firebase';
-import './partials/js/library';
+
