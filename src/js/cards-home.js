eb1535963675d2a93498875e8137d4c3b8b6d599
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-02-08 18:35:31 +0200

    homepage cards

--- src/js/cards-home.js
+++ src/js/cards-home.js
@@ -1,14 +1,23 @@
+import { getGenresList } from './getGenres';
+const BASE_SRC = 'https://image.tmdb.org/t/p/original/';
+
 export const createHomepageCards = results => {
   const markup = results
-    .map(el => {
+    .map(({ id, poster_path, title, release_date, genre_ids }) => {
+      const date = new Date(release_date);
+      const fullYear = date.getFullYear();
+      const genresList = getGenresList(genre_ids);
       return `
-      <li class="card" data-id="{1234}>
-          <img class="card__img" src="./images/logo.png" alt="{poster}" data-source="{original}"  loading="lazy">
-          <h2 class="card__title">{GREYHOUND}</h2>
-          <p class="card__text">{Drama, Action} | {2020}</p>
+      <li class="card" data-id="${id}">
+          <img class="card__img" src="${BASE_SRC}${poster_path}" alt="${title}" loading="lazy">
+          <h2 class="card__title">${title}</h2>
+          <p class="card__text">${genresList} | ${fullYear}</p>
       </li>`;
     })
     .join('');
 
   return markup;
 };
+
+// // TO DO
+// // falback for pictures
