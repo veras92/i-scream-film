f78eb4204de8f63aa168b30105bf6231744dfca2
AnttonnIT <anttonnit@gmail.com>
2023-02-13 21:24:06 +0200

    Update authentication.js
    fixed

--- src/js/authentication.js
+++ src/js/authentication.js
@@ -66,37 +66,46 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 function emailAuthentication() {
   modalContentEl.classList.add('logging--is-hiden');
   firebaseContainerEl.classList.remove('logging--is-hiden');
+  firebaseContainerEl.classList.remove('display-none');
 
   ui.start('#firebaseui-auth-container', {
+    callbacks: {
+      signInFailure: function (error) {
+        // Some unrecoverable error occurred during sign-in.
+        // Return a promise when error handling is completed and FirebaseUI
+        // will reset, clearing any UI. This commonly occurs for error code
+        // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+        // occurs. Check below for more details on this.
+        return handleUIError(error);
+      },
+    },
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    // Query parameter name for mode.
+    queryParameterForWidgetMode: 'mode',
+    // Query parameter name for sign in success url.
+    queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+
       {
         provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+        // Whether the display name should be displayed in the Sign Up page.
+        requireDisplayName: true,
       },
     ],
-
-    callbacks: {
-      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-        backDropEl.classList.add('logging--is-hiden');
-        btnLoginEl.classList.add('logging--is-hiden');
-        linkLibraryEl.classList.remove('logging--is-hiden');
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return false;
-      },
-      // Other config options...
-    },
   });
 }
 
 // модальне вікно логіну
 btnLoginEl.addEventListener('click', () => {
   backDropEl.classList.remove('logging--is-hiden');
+  emailAuthentication();
   window.addEventListener('keydown', onEscKeyPress);
 });
-btnGoogleEl.addEventListener('click', googleAuthentication);
-btnEmailEl.addEventListener('click', emailAuthentication);
 
 closeAuthBtn.addEventListener('click', () => {
   backDropEl.classList.add('logging--is-hiden');
